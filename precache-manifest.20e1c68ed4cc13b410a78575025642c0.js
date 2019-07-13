87bc9cd9a0d6ae474660edf11049071cf4bcedd9
cuongle30 <cuongle30@gmail.com>
2019-07-13 13:03:52 -0700

    Updates

---
+++ precache-manifest.20e1c68ed4cc13b410a78575025642c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bcb0a0ad025aba244cba1cc7f719598",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "5de09dcc10ad95ef98bf",
+    "url": "/ClickyGame/static/css/main.5acd67c8.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/ClickyGame/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "5de09dcc10ad95ef98bf",
+    "url": "/ClickyGame/static/js/main.ee9dd69b.chunk.js"
+  },
+  {
+    "revision": "95fffdb9eb36913c2817",
+    "url": "/ClickyGame/static/js/runtime~main.1eb9bb0a.js"
+  }
+]);
